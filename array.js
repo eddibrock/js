e125d29078b998c32877720717ab42bc8e1cefcf
Sergey Abakaev (PC) <abakaev_sergey93@mail.ru>
2020-01-13 21:26:43 +0700

    types

--- array.js
+++ array.js
@@ -1,76 +1,69 @@
 /** Bool */
-
 //Array.isArray();
 /** Изменяющие исходный массив */
-function spliceIt(arr) {
-    // arr.splice(0,1); // с позиции 0 удалить 1 елемент
-    // arr.splice(0,1,'one.half'); // с позиции 0 удалить 1 елемент и добавить 'one.half'
-    // arr.splice(0,0,'one.half'); // с позиции 0 добавить 'one.half'
-    // let sliced=arr.splice(1); // Вырезать массив с 1 елемента включительно и передать кусок в новый массив
-    const addition = ['five', 'six'];
-    // arr.splice(arr.length,0,...addition); // в конец массива добавить массив
-}
+let arrChangeObj = {
+    splice: function (arr) {
+        /** Просто вырезание */
+        // arr.splice(0,1); // с позиции 0 удалить 1 елемент (включительно)
+        /** Добавление */
+        const addition = ['five', 'six'];
+        // arr.splice(0,1,addition[0]); // с позиции 0 удалить 1 елемент (включительно) и добавить 'one.half'. Можно и не удалять тогда 0 вместо 1 будет
+        // arr.splice(arr.length,0,...addition); // в конец массива добавить массив
+    },
+    /** Сортировка в массиве */
+    sortIt: function () {
+        let unsortedNum = [1, 3, 5, 2, 14]; // Если сортируем числа
+        let unsortedStr = ['C', 'b', 'a',]; // Можно сортировать и строки, но Заглавная буква вносит свои корректировки
+        unsortedStr.sort(); // сортирует как строки и 2>14 из за этого + сортирует сам массив
+        //поэтому лучше использовать свою функцию для сортировки
+        unsortedNum.sort((a, b) => a - b); //Если А больше B то вернется положительное число
+        console.log(unsortedStr);
+        //TODO тема расскрыта не до конца https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+    },
+    reverseIt: function (arr) {
+        arr.reverse(); // изменяет оригинальй массив;
+    },
+    pushPopShiftUnshift: function () {
+        let someElem = 'hi';
+        let someArr = ['here', 'we', 'go'];
+        someArr.push(someElem); //добавили hi справа, теперь [ 'here', 'we', 'go', 'hi' ]
+        someArr.pop(); // убрали hi справа
+        someArr.shift(); // Убрали елемент слева, теперь [ 'we', 'go' ]
+        someArr.unshift(someElem); // Добавили hi слева, теперь [ 'hi', 'we', 'go' ]
+    }
 
-/** Сортировка в массиве */
-function sortIt() {
-    let unsortedNum = [1, 3, 5, 2, 14]; // Если сортируем числа
-    let unsortedStr = ['C', 'b', 'a',]; // Можно сортировать и строки, но Заглавная буква вносит свои корректировки
-    unsortedStr.sort(); // сортирует как строки и 2>14 из за этого + сортирует сам массив
-    //поэтому лучше использовать свою функцию для сортировки
-    unsortedNum.sort((a, b) => a - b); //Если А больше B то вернется положительное число
-    console.log(unsortedStr);
-    //TODO тема расскрыта не до конца https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-}
-
-function reverseIt(arr) {
-    arr.reverse(); // изменяет оригинальй массив;
-}
-
-function pushPopShiftUnshift() {
-    let someElem = 'hi';
-    let someArr = ['here', 'we', 'go'];
-    someArr.push(someElem); //добавили hi справа
-    console.log(someArr); // теперь [ 'here', 'we', 'go', 'hi' ]
-    someArr.pop(); // убрали hi справа
-    someArr.shift(); // Убрали елемент слева
-    console.log(someArr); // теперь [ 'we', 'go' ]
-    someArr.unshift(someElem); // Добавили hi слева
-    console.log(someArr); //[ 'hi', 'we', 'go' ]
-}
+};
 
 /** НЕ изменяющие исходный массив но создающие новый*/
-function sliceIt(arr) {
-    /** в отличии от метода splice этот метод не изменяет исходный массив */
+let arrUnchangeObj = {
+    sliceIt: function (arr) {
+        /** в отличии от метода splice этот метод не изменяет исходный массив */
         // const newArr=arr.slice(); //скопировать массив
         // const newArr=arr.slice(1,3); //скопировать с 1 позиции включительно и до 3 НЕ включительно
-    const newArr = arr.slice(-1); //скопировать с 1 елемент с конца
-    console.log(newArr);
-}
+    },
+    concatIt: function (arr) {
+        const addition = ['five', 'six'];
+        const newArr = arr.concat(addition, 'seven');
+        // const newArr=Array.prototype.concat(arr,addition);
+        console.log(newArr);
+    },
+    splitJoin: function () {
+        let str = 'abcd';
+        let arr = str.split('');
+        let glued = arr.join('/');
+        console.log(glued);
+    },
 
-function concatIt(arr) {
-    const addition = ['five', 'six'];
-    const newArr = arr.concat(addition, 'seven');
-    // const newArr=Array.prototype.concat(arr,addition);
-    console.log(newArr);
-}
+    /** Поиск в массиве */
+    searchIt: function () {
+        let arr = [1, 0, false];
 
-function splitJoin() {
-    let str = 'abcd';
-    let arr = str.split('');
-    let glued = arr.join('/');
-    console.log(glued);
-}
-
-/** Поиск в массиве */
-function searchIt() {
-    let arr = [1, 0, false];
-
-    alert(arr.indexOf(0)); // 1
-    alert(arr.indexOf(false)); // 2
-    alert(arr.indexOf(null)); // -1
-
-    alert(arr.includes(1)); // true
-}
+        alert(arr.indexOf(0)); // 1
+        alert(arr.indexOf(false)); // 2
+        alert(arr.indexOf(null)); // -1
+        alert(arr.includes(1)); // true
+    }
+};
 
 
 /** find findIndex map reduce (reduceEnd) filter
@@ -79,8 +72,8 @@ function searchIt() {
  * */
 function mostUsedMethods() {
     const arr = [1, 2, 3, 4, 5, 2];
-    const isSomeInArr=arr.some(el=>el===3); // хотябы 1 равен 3
-    const isAnyInArr=arr.every(el=>el<6); // все меньше 6
+    const isSomeInArr = arr.some(el => el === 3); // хотябы 1 равен 3
+    const isAnyInArr = arr.every(el => el < 6); // все меньше 6
     /**поиск и фильтрация*/
     const findValue = arr.find(item => item === 2); // Если елемент равен 2 то он будет значением переменной а поиске прекратится
     const findIdx = arr.findIndex(item => item === 2); // Тоже самое, но уже возвращается не значение а индекс
@@ -91,18 +84,18 @@ function mostUsedMethods() {
     }); // Получить только четные числа. Работает так - если return true то item попадет в массив. При этом, как и в map и reduce, можно что то делать
     // с другими аргументами, например с index(получить массив индексов четных чисел для дальнейшего доступа к ним). Но обязательно нужно вернуть TRUE иначе получим пустой массив
     /**действие над елементами массива*/
-    const multiplyBy2 = arr.map(item => item*2); //делает что то с елементом и копирует результат в новый массив
-    const reduceValue=arr.reduce((acc,el)=>acc+el,0); // просто суммируем каждое число вместе.
+    const multiplyBy2 = arr.map(item => item * 2); //делает что то с елементом и копирует результат в новый массив
+    const reduceValue = arr.reduce((acc, el) => acc + el, 0); // просто суммируем каждое число вместе.
 }
+
 
 /** Циклы */
+// for(let word of arr){
+//     console.log(word);
+// }
 let arr = ['one', 'two', 'tree', 'for'];
-
-for(let word of arr){
-    console.log(word);
-}
-
-mostUsedMethods();
+console.log(arr);
+// mostUsedMethods();
 // pushPopShiftUnshift();
 // splitJoin();
 // sortIt();
--- test.js
+++ test.js
@@ -1,22 +1,15 @@
-let arr =
-    [[1,2,3], //     [7, 4, 1]
-        [4,5,6], //  [8, 5, 2]
-        [7,8,9]]; // [9, 6, 3]
-//[[7,4,1],
-//  [8,5,2],
-//  [9,6,3]]
-let res = [];
-for(let elem of arr){
-    console.log(elem);
-    res.push([elem[0]]); //создаем новый массив с первым елементом
+//найти крайний правый 0 в двоичном представлении числа 37 и получить его индекс
 
+//возвести 2 в степень равную степени индекса
+
+
+function findIndexCustom(int) {
+    let arr = int.toString(2).split('').reverse();
+    console.log(arr);
+    let pos = arr.findIndex(el => el == 0);
+    let posSecond = arr.findIndex((el,index) => el == 0 && index!==1);
+    console.log(posSecond);
+    return 2**(posSecond+1);
 }
-console.log(res);
 
-let newArr = arr.map((item, index, array) => {
-    for (let i = 1; i < item.length; i++) {
-        res[i].unshift(item[i]);
-    }
-});
-
-// console.log(res);
+console.log(findIndexCustom(1073741824));
